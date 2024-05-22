b8864cba10e75d0a40adada7010ae0fb19697b1e
IlyaKha <powerfulkha96@mail.ru>
2024-05-22 15:29:51 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -6,12 +6,17 @@ students_list = list(students)
 
 # Словарь для хранения средних баллов
 average_grades = {}
-
+students_list.sort()   # сортировка в алфаыитном порядке
 # Перебираем списки оценок и имена учеников
 for i in range(len(grades)):
+
     # Вычисляем средний балл для текущего ученика
     average_grade = sum(grades[i]) / len(grades[i])
 
     # Добавляем в словарь имя ученика как ключ и средний балл как значение
     average_grades[students_list[i]] = average_grade
 print(average_grades)
+
+
+
+
